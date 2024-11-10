2ffb19744ef70fb17f8d57b93c671b5db0cab549
X13X666X <newmicrogamer@gmail.com>
2024-11-11 00:22:46 +0200

    upload

---
+++ lab4.2/lab4.2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x, xp, xk, dx, A, B, y;
+    cout << "xp: "; cin >> xp;
+    cout << "xk: "; cin >> xk;
+    cout << "dx: "; cin >> dx;
+
+    cout << fixed;
+    cout << "---------------------------" << endl;
+    cout << "|" << setw(5) << "x" << " |" << setw(10) << "y" << " |" << endl;
+    cout << "---------------------------" << endl;
+
+    x = xp;
+    while (x <= xk)
+    {
+        A = 2 * x * x;
+
+        if (x <= -0.1)
+            B = 5 * sin(3 * x);
+        else if (x < 1.2)
+            B = atan((x + 1) / 4);
+        else
+            B = tan(x) + 2;
+
+        y = A - B;
+
+        cout << "|" << setw(7) << setprecision(2) << x
+            << " |" << setw(10) << setprecision(3) << y << " |" << endl;
+
+        x += dx;
+    }
+
+    cout << "---------------------------" << endl;
+    return 0;
+}
